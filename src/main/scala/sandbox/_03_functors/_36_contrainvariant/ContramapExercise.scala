c95a542ec8bbf9c306f937991cf5284319036668
Hermann Hueck <hermann.hueck@mailbox.org>
2018-05-29 12:15:38 +0200

    contramap exercise improved

--- src/main/scala/sandbox/_03_functors/_36_contrainvariant/ContramapExercise.scala
+++ src/main/scala/sandbox/_03_functors/_36_contrainvariant/ContramapExercise.scala
@@ -38,22 +38,32 @@ object ContramapExercise extends App {
 
   final case class Box[A](value: A)
 
-/*
   // 1. solution based on the Printable[A]
-  implicit def boxPrintable[A](implicit p: Printable[A]): Printable[Box[A]] =
-    new Printable[Box[A]] {
-      def format(box: Box[A]): String =
-        "[" + p.format(box.value) + "] in a Box"
-    }
-*/
+  {
+    println("--- Printable for Box[A]: 1. solution based on the Printable[A]")
+
+    implicit def boxPrintable[A](implicit p: Printable[A]): Printable[Box[A]] =
+      new Printable[Box[A]] {
+        def format(box: Box[A]): String =
+          "[" + p.format(box.value) + "] in a Box"
+      }
+
+    println(format(Box(15)))
+    println(format(Box("hello world")))
+    println(format(Box(true)))
+  }
 
   // 2. solution with contramap
-  implicit def boxPrintable[A](implicit pa : Printable[A]): Printable[Box[A]] =
-    pa.contramap[Box[A]](_.value)
+  {
+    println("--- Printable for Box[A]: 2. solution with contramap")
 
-  println(format(Box(15)))
-  println(format(Box("hello world")))
-  println(format(Box(true)))
+    implicit def boxPrintable[A](implicit pa: Printable[A]): Printable[Box[A]] =
+      pa.contramap[Box[A]](_.value)
+
+    println(format(Box(15)))
+    println(format(Box("hello world")))
+    println(format(Box(true)))
+  }
 
   println("---")
 }
