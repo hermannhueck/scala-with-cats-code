ee584a41c0fbc910404e6c18d3152bfbb253e1cd
Hermann Hueck <hermann.hueck@mailbox.org>
2018-05-29 12:13:06 +0200

    improved definition of Monoid

--- src/main/scala/sandbox/_02_monoids/Monoid.scala
+++ src/main/scala/sandbox/_02_monoids/Monoid.scala
@@ -5,6 +5,28 @@ trait Monoid[A] extends Semigroup[A] {
 }
 
 object Monoid {
-  def apply[A](implicit monoid: Monoid[A]) =
-    monoid
+  // def apply[A](implicit monoid: Monoid[A]): Monoid[A] = monoid
+  def apply[A : Monoid]: Monoid[A] = implicitly
 }
+
+abstract class MonoidAdapter[A](override val empty: A)(join: Function2[A, A, A]) extends Monoid[A] {
+  override def combine(x: A, y: A): A = join(x, y)
+}
+
+object MonoidInstances {
+
+  implicit object SumMonoid extends MonoidAdapter[Int](empty = 0)(join = _ + _)
+
+  implicit object ProductMonoid extends MonoidAdapter[Int](empty = 1)(join = _ * _)
+
+  implicit object StringMonoid extends MonoidAdapter[String](empty = "")(join = _ + _)
+
+  implicit object AllMonoid extends MonoidAdapter[Boolean](empty = true)(join = _ && _)
+
+  implicit object AnyMonoid extends MonoidAdapter[Boolean](empty = false)(join = _ || _)
+
+  abstract class ListMonoid[A] extends MonoidAdapter[List[A]](empty = List.empty[A])(join = _ ++ _)
+
+  implicit object IntListMonoid extends ListMonoid[Int]
+}
+
